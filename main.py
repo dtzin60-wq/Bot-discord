1474be1e5135ba63d7d35137288b7b639ede953c
dtzin60-wq <dtzin60@gmail.com>
2026-01-29 19:36:54 -0300

    main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord
+ import discord
 from discord.ext import commands
 from discord.ui import View, Button, Modal, TextInput, RoleSelect, ChannelSelect, Select
 import sqlite3, os, random, asyncio
